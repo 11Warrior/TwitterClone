fafa6d3a2c31934a5765bc84bd5a17f044d7b609
Aayush <aayushthapa640@gmail.com>
2025-04-19 16:54:25 +0530

    final lets hope

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
     queryKey: ['authUser'],
     queryFn: async() => {
       try {
-				const res = await fetch("/api/auth/getAuthUser");
+				const res = await fetch(`${import.meta.env.VITE_API_BASE_URL}/auth/getAuthUser`);
 				const user = await res.json();
 				if (user.error) return null;
 				if (!res.ok) {
