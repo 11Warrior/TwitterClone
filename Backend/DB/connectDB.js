6bd039c0915397e5c03aab815d6e0998d812d835
Aayush <aayushthapa640@gmail.com>
2025-04-19 15:12:50 +0530

    ready to deploy

--- Backend/DB/connectDB.js
+++ Backend/DB/connectDB.js
@@ -1,5 +1,6 @@
+import { config } from "dotenv";
 import mongoose from "mongoose";
-
+config()
 export const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI).then(console.log('Connected to our database'))
--- Backend/server.js
+++ Backend/server.js
@@ -1,20 +1,28 @@
 import express from 'express'
+
 import authRoutes from './routes/auth.routes.js'
 import userRouter from './routes/user.routes.js'
 import postRoutes from './routes/post.routes.js'
 import notificationRoutes from './routes/notification.route.js'
 
-import  {config}  from 'dotenv'
 import { connectDB } from './DB/connectDB.js';
 import cookieParser from 'cookie-parser';
 import {v2 as cloudinary} from 'cloudinary'
 
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
+
+
 const app = express()
-config();
 
 app.use(express.json({limit: "10mb"}));
 app.use(express.urlencoded({extended : true}))
 app.use(cookieParser())
+
 
 app.use("/api/auth", authRoutes)
 app.use("/api/users", userRouter)
@@ -30,12 +38,15 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
   secure: true
-})
+});
 
 
 connectDB();
-const port = process.env.PORT;
 
+app.get('/', (req, res) => {
+  res.send('Server is up');
+});
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}...`);
 });
